993afc8cdf18fe5bd7a3e9bdd461a0d640cfcc1e
LastSamurai <31820715+LastSamurai333@users.noreply.github.com>
2019-04-13 02:02:59 +0530

    Update serving_sample_request.py

--- scripts/serving_sample_request.py
+++ scripts/serving_sample_request.py
@@ -23,7 +23,7 @@ payload = {
 }
 
 # sending post request to TensorFlow Serving server
-r = requests.post('http://localhost:5454/v1/models/ImageClassifier:predict', json=payload)
+r = requests.post('http://localhost:2000/v1/models/ImageClassifier:predict', json=payload)
 pred = json.loads(r.content.decode('utf-8'))
 
 # Decoding the response
