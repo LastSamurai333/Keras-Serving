40bd9ef865fffdb2b571740c946f7cbd10dc862f
LastSamurai <31820715+LastSamurai333@users.noreply.github.com>
2019-04-13 10:05:33 +0530

    Update serving_sample_request.py

--- scripts/serving_sample_request.py
+++ scripts/serving_sample_request.py
@@ -23,10 +23,18 @@ payload = {
 }
 
 # sending post request to TensorFlow Serving server
-r = requests.post('http://localhost:2000/v1/models/ImageClassifier:predict', json=payload)
+r = requests.post('http://localhost:1234/v1/models/ImageClassifier:predict', json=payload)
 pred = json.loads(r.content.decode('utf-8'))
+
+dict['Sleeveless'] = pred['predictions'][0][0]
+dict['FullSleeve'] = pred['predictions'][0][0]
+dict['HalfSleeve'] = pred['predictions'][0][0]
+dict['3/4 Sleeve'] = pred['predictions'][0][0]
+y = json.dumps(dict)
+
+print (y)
 
 # Decoding the response
 # decode_predictions(preds, top=5) by default gives top 5 results
 # You can pass "top=10" to get top 10 predicitons
-print(json.dumps(model.decode_predictions(np.array(pred['predictions']))[0]))
+# print(json.dumps(model.decode_predictions(np.array(pred['predictions']))[0]))
