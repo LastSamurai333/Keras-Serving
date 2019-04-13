546cbebe05cb041ff9e6c3fa9bcb6a2c50b2429a
LastSamurai <31820715+LastSamurai333@users.noreply.github.com>
2019-04-13 11:58:37 +0530

    Update export_saved_model.py

--- scripts/export_saved_model.py
+++ scripts/export_saved_model.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 # The export path contains the name and the version of the model
 tf.keras.backend.set_learning_phase(0)  # Ignore dropout at inference
-model = tf.keras.models.load_model('./inception.h5')
+model = tf.keras.models.load_model('/content/inception.h5')
 export_path = '/content/keras-serving/my_image_classifier/1'
 
 # Fetch the Keras session and save the model
